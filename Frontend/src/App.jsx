35558204a07326a80366f4d45ff501c1199afff4
Hafiz Hamza <khawajahamza566@gmail.com>
2025-09-20 17:24:21 +0500

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
+import { useState } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -10,6 +11,12 @@ import ProtectedRoute from "./ProtectedRoute";
 import "./App.css";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNavClick = () => {
+    setMenuOpen(false); // Close menu when clicking a link
+  };
+
   return (
     <Router>
       <div className="app">
@@ -22,25 +29,54 @@ function App() {
                 {/* Navbar */}
                 <nav className="navbar">
                   <div className="logo">EarnOnline</div>
-                  <div className="menu-icon">☰</div>
-                  <ul className="nav-links">
+
+                  {/* Hamburger Icon */}
+                  <div
+                    className="menu-icon"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                  >
+                    ☰
+                  </div>
+
+                  {/* Nav Links */}
+                  <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
                     <li>
-                      <ScrollLink to="home" smooth duration={500}>
+                      <ScrollLink
+                        to="home"
+                        smooth
+                        duration={500}
+                        onClick={handleNavClick}
+                      >
                         Home
                       </ScrollLink>
                     </li>
                     <li>
-                      <ScrollLink to="about" smooth duration={500}>
+                      <ScrollLink
+                        to="about"
+                        smooth
+                        duration={500}
+                        onClick={handleNavClick}
+                      >
                         About
                       </ScrollLink>
                     </li>
                     <li>
-                      <ScrollLink to="blog" smooth duration={500}>
+                      <ScrollLink
+                        to="blog"
+                        smooth
+                        duration={500}
+                        onClick={handleNavClick}
+                      >
                         Blog
                       </ScrollLink>
                     </li>
                     <li>
-                      <ScrollLink to="contact" smooth duration={500}>
+                      <ScrollLink
+                        to="contact"
+                        smooth
+                        duration={500}
+                        onClick={handleNavClick}
+                      >
                         Contact
                       </ScrollLink>
                     </li>
